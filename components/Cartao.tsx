a470bab5d920fbf8bf169e130db520323dd34d56
Gabriel Souza <rorato91@gmail.com>
2023-01-27 11:13:32 -0300

    Initial commit

---
+++ components/Cartao.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Cartao.module.css"
+
+interface CartaProps {
+  bgcolor?: string
+  children?: any
+}
+
+export default function Cartao(props: CartaProps) {
+  return (
+    <div className={styles.cartao} style={{ backgroundColor: props.bgcolor ?? "#FFF" }}>
+      {props.children}
+    </div>
+  )
+}
